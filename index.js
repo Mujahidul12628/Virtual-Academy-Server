15c6e6ca0ef60c4d8dcb22aa526e6c49df9e94ba
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-10-19 12:18:43 +0600

    Index and JSON data

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+const cors = require("cors");
+
+const port = process.env.PORT || 5700;
+
+app.use(cors());
+
+const campClassData = require("./campData.json");
 
 app.get('/', (req, res) => {
-    res.send('Virtual Academy Server');
+    res.send('Summer Camp Server');
+});
+
+app.get('/campData', (req, res) => {
+    res.send(campClassData);
+});
+
+app.get('/campData/:id', (req, res) => {
+    const campId = req.params.id;
+    const campData = campClassData.find(camp => camp.id === parseInt(campId));
+    res.send(campData);
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server listening on port ${port}`);
 });
