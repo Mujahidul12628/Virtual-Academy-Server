c0ae3c608ddda745556e91675613a9096ca57553
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-10-25 11:01:50 +0600

    JSON updated

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const app = express();
+// const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const cors = require("cors");
 
 const port = process.env.PORT || 5700;
+// const uri = "mongodb+srv://virtualacademy628:OnoPSTWepxqJwLHs@cluster0.xuczi3d.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     }
+// });
+
 
 app.use(cors());
 
 const campClassData = require("./campData.json");
 
 app.get('/', (req, res) => {
-    res.send('Summer Camp Server');
+    res.send('Virtual Academy Server');
 });
 
 app.get('/campData', (req, res) => {
@@ -22,6 +32,20 @@ app.get('/campData/:id', (req, res) => {
     const campData = campClassData.find(camp => camp.id === parseInt(campId));
     res.send(campData);
 });
+
+// async function run() {
+//     try {
+//         // Connect the client to the server	(optional starting in v4.7)
+//         await client.connect();
+//         // Send a ping to confirm a successful connection
+//         await client.db("virtualAcademies").command({ ping: 1 });
+//         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//     } finally {
+//         // Ensures that the client will close when you finish/error
+//         await client.close();
+//     }
+// }
+// run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
